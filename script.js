24c0cacee9c96c22276ac39a8cc8d2aab389ed56
Nguyen Trinh <khanhnguyentrinhthi@gmail.com>
2025-02-14 21:54:25 -0500

    Update structure

--- script.js
+++ script.js
@@ -1,4 +1,135 @@
+let data ={
+    "pageDescription": [
+        {
+            "page": "about",
+            "content":"<p>about page</p>"
+        },
+        {
+            "page": "experience",
+            "content": {
+                "experience":[
+                {
+                    "role": "Undergraduate Math Tutor",
+                    "organization": "Drexel University, Math Resource Center",
+                    "year": "2025",
+                    "description":"I taught stuffs to people"
+                },
+                {
+                    "role": "Robotics Research Intern",
+                    "organization": "Makerspace, Fulbright University Vietnam",
+                    "year": "2023",
+                    "description":"yap yap"
+                },
+                {
+                    "role": "Lead Programmer, Vietnam Representative",
+                    "organization": "World Robot Olympiad",
+                    "year": "2022",
+                    "description":"yap yap"
+                }],
+                "education": [
+                {
+                    "degree": "Computer Science, B.S.",
+                    "school": "Drexel University",
+                    "GPA":"4.0/4.0",
+                    "start-year": "2024",
+                    "end-year": "2029",
+                    "description":"yap yap"
+                },
+                {
+                    "degree": "Mathematics Specialized, High School Diploma",
+                    "school": "VNU-HCM High School for the Gifted",
+                    "GPA":"9.7/10.0",
+                    "start-year": "2021",
+                    "end-year": "2024",
+                    "description":"yap yap"
+                }]
+            }
+        },
+        {
+            "page": "projects",
+            "intro": "Working on projects is my favorite thing to do: it allows me to apply what I’ve learned, pick up new skills, solve problems creatively, and come up with a meaningful final product. I used multiple different languages and tools for each project to diversify my skill set and understand the strengths of each technology.</p><p>Take a look at what I've been creating below!",
+            "major-projects": [
+                "PAL",
+                "Soud'n'Soar",
+                "PII-Robot",
+                "WRO'22"
+            ],
+            "side-projects": [
+                "Virtual-Pet",
+                "Desksert",
+                "Mr.Discipline"
+            ],
+            "note": "Also, I love spending time on side quests, bringing some of my quirky ideas to life as mini-projects, purely out of the joy of making something unique on my own."
+        },
+        {
+            "page": "skills",
+            "categories": [
+                {
+                    "category": "Languages",
+                    "tags": ["Python", "Java", "C", "JavaScript", "HTML/CSS", "SQL", "MATLAB", "R"]
+                },
+                {
+                    "category": "Frameworks",
+                    "tags": ["React", "Node.js", "Express", "Flask", "Django", "Bootstrap", "Material-UI"]
+                },
+                {
+                    "category": "Tools",
+                    "tags": ["Git", "GitHub", "GitLab", "VS Code", "XCode", "Figma"]
+                }
+            ]
+        },
+        {
+            "page": "contact",
+            "content": "Hit me up!"
+        },
+        {
+            "page": "random",
+            "content": [
+                "Hey, I'm currently listening to this song!"
+            ]
+        }
+    ],
+    "projects": [
+        {
+            "name": "PAL",
+            "subhead": "A something project",
+            "description": "bla bla",
+            "techtags": ["Python", "Java", "C", "JavaScript", "HTML/CSS", "SQL", "MATLAB", "R"],
+            "link": "https://github.com/nguyen-trinhtk/"
+        }, 
+        {
+            "name": "Soud'n'Soar",
+            "subhead": "A something project",
+            "description": "bla bla",
+            "techtags": ["Python", "Java", "C", "JavaScript", "HTML/CSS", "SQL", "MATLAB", "R"],
+            "link": "https://github.com/nguyen-trinhtk/"
+        },
+        {
+            "name": "PII-Robot",
+            "subhead": "A something project",
+            "description": "bla bla",
+            "techtags": ["Python", "Java", "C", "JavaScript", "HTML/CSS", "SQL", "MATLAB", "R"],
+            "link": "https://github.com/nguyen-trinhtk/"
+        },
+        {
+            "name": "WRO'22",
+            "subhead": "A something project",
+            "description": "bla bla",
+            "techtags": ["Python", "Java", "C", "JavaScript", "HTML/CSS", "SQL", "MATLAB", "R"],
+            "link": "https://github.com/nguyen-trinhtk/"
+        }
+    ]
+}
 
+const aboutObj = data.pageDescription.find(page => page.page === "about");
+const experienceObj = data.pageDescription.find(page => page.page === "experience");
+const projectsObj = data.pageDescription.find(page => page.page === "projects");
+const skillsObj = data.pageDescription.find(page => page.page === "skills");
+const contactObj = data.pageDescription.find(page => page.page === "contact");
+const randomObj = data.pageDescription.find(page => page.page === "random");
+
+
+//draggable icons/pages
 function makeElementDraggable(element) {
     let isDragged = false;
     let startX, startY;
@@ -49,35 +180,49 @@ function makeElementDraggable(element) {
     }
 }
 
+//show page's content in terminal
 function showContent(id) {
     terminalBody.innerHTML += `<p>${cmdStart} <span class="blue bold">~${cwd}</span>&#65284; cd ${id} </p>`;
     cwd = `/${id}`;
     terminalBody.innerHTML += `<p>${cmdStart} <span class="blue bold">~${cwd}</span>&#65284; cat ${id}.txt </p>`;
     if (id == "about") {
-        terminalBody.innerHTML += 
-        '<p>Hi, I’m Nguyen Trinh. I’m currently a first-year computer science undergrad at Drexel University. I’m interested in software development [hover: web app, mobile app], robotics [hover: autonomous navigation, embedded systems, IoT], and math [hover: modeling, analysis, optimization], but anything can be my rabbit hole. </p><p>Earlier, I worked as a robotics research intern at Fulbright University Vietnam’s Makerspace and was a representative of Vietnam at the 2022 World Robot Olympiad. I’m also committed to supporting equity in tech as an active member of Rewriting the Code.</p><p>In my own time, you’ll see me either doing digital paintings, going for a walk and taking photographs, Sporcle-ing, or organizing my Spotify playlist. If not, I’m probably busy enjoying a big hot bowl of Pho~</p>';                
-    
+        terminalBody.innerHTML += `<p>${aboutObj.content}</p>`;               
     }
     else if (id == "experience") {
-        terminalBody.innerHTML += 
-        '<p><span class="bold">Robotics Research Intern</span> at Fulbright University Vietnam’s Makerspace</p><p><span class="bold">Representative</span> of Vietnam at the 2022 World Robot Olympiad</p><p><span class="bold">Member</span> of Rewriting the Code</p>';
+        experienceObj.content.experience.forEach(item => {
+            terminalBody.innerHTML += `<span>|<br><b>[${item.year}]</b>[<span class="blue">${item.role}</span> @ <span class="green">${item.organization}]<br></span></span>`;
+            terminalBody.innerHTML += `<span>${item.description}</span><br>`;
+            terminalBody.innerHTML += `|<br>`;
+          });
     }
     else if (id == "projects") {
-        terminalBody.innerHTML += "<p>Working on projects is my favorite thing to do: it allows me to apply what I’ve learned, pick up new skills, solve problems creatively, and come up with a meaningful final product. I used multiple different languages and tools for each project to diversify my skill set and understand the strengths of each technology.</p><p>Take a look at what I've been creating below!</p><div class='project-link-container'><button onclick='showProject()' class='project-link underline'>PAL</button><button onclick='showProject()' class='project-link underline'>Soud'n'Soar</button><button onclick='showProject()' class='project-link underline'>PII Robot</button><button onclick='showProject()' class='project-link underline'>WRO'22</button></div><p>Also, I <b>love</b> spending time on side quests, bringing some of my quirky ideas to life as mini-projects, purely out of the joy of making something unique on my own.</p><div class='project-link-container'><button onclick='showProject()' class='project-link underline'>Virtual Pet</button><button onclick='showProject()' class='project-link underline'>Desksert</button><button onclick='showProject()' class='project-link underline'>Mr.Discipline</button></div>";
+        terminalBody.innerHTML += `<p>${projectsObj.intro}</p>`;
+        projectsObj["major-projects"].forEach(project => {
+            terminalBody.innerHTML += `<b class='underline green project-item' onclick="showProjectDetails('${project}')">${project}</b>`;
+        });
+        terminalBody.innerHTML += `<p>${projectsObj.note}</p>`;
+        projectsObj["side-projects"].forEach(project => {
+            terminalBody.innerHTML += `<b class='underline green project-item' onclick="showProjectDetails('${project}')">${project}</b>`;
+        });
     }
     else if (id == "skills") {
+        skillsObj.categories.forEach(category => {
+            terminalBody.innerHTML += `<span><b>[${category.category}]</b><br></span>`;
+            category.tags.forEach(tag => {
+              terminalBody.innerHTML += `<span class="skill">${tag}</span>`;
+            });
+            terminalBody.innerHTML += `<br>`;
+          });
     }
     else if (id == "contact") {
     }
     else if (id == "random") {
     }
     terminalBody.innerHTML += '<button onclick="clearTerminal()" class="close-btn underline"><< Back to home</button>';
-    terminalBody.innerHTML += `<p>${cmdStart} <span class="blue bold">~${cwd}</span>&#65284;`;
+    // terminalBody.innerHTML += `<p>${cmdStart} <span class="blue bold">~${cwd}</span>&#65284;`;
 }
 
-function showProject() {
-}
-
+//responsive terminal title
 function setTerminalTitle() {
     let terminalTitle = document.getElementById("terminal-title");
     let terminalWindow = document.getElementById("terminal-window");
@@ -89,6 +234,8 @@ function setTerminalTitle() {
     updateTitle();
 }
 
+
+//terminal functions
 function hideTerminal() {
     let paragraphs = terminalBody.getElementsByTagName('p');
     if (paragraphs.length > 0) {
@@ -133,7 +280,29 @@ function maximizeTerminal() {
     }, 100);
 }
 
+function showProjectDetails(projectName) {
+    const project = data.projects.find(proj => proj.name === projectName);
+    if (!project) return;
 
+    // Clear terminal
+    terminalBody.innerHTML = "";
+
+    // Display project details
+    terminalBody.innerHTML += `<p>${cmdStart} <span class="blue bold">~${cwd}</span>&#65284; cd ${projectName}</p>`;
+    cwd = `/projects/${projectName}`;
+    terminalBody.innerHTML += `<p>${cmdStart} <span class="blue bold">~${cwd}</span>&#65284; cat ${projectName}.txt </p>`;
+    terminalBody.innerHTML += `<h3>${project.name} - ${project.subhead} <a href="${project.link}" target="_blank" class="project-link">&#x2197;</a></h3>`;
+    terminalBody.innerHTML += `<p>${project.description}</p>`;
+    terminalBody.innerHTML += `<u>Tech stack</u>:`;
+    project.techtags.forEach(tag => {
+        terminalBody.innerHTML += `<span class="skill">#${tag}</span>`;
+      });
+
+    // Back button
+    terminalBody.innerHTML += '<button onclick="clearTerminal()" class="close-btn underline"><< Back to home</button>';
+}
+
+//initializing variables
 let about = document.getElementById("about");
 let experience = document.getElementById("experience");
 let projects = document.getElementById("projects");
